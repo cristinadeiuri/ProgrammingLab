a717bf651fbc953809f385ec09288a7a1167f326
deiuricristina <94189313+cristinadeiuri@users.noreply.github.com>
2022-02-04 15:38:16 +0000

    file (somma.py) che stampa a schermo i valori di una lista

---
+++ somma.py
@@ -0,0 +1,10 @@
+#scrivere una funzione che sommi tutti gli elementi di una lista
+
+my_list = [2,5,6]
+
+for item in my_list:#stampo a schermo gli elementi della mia lista, non è necessario ai fini dell'esercizio, volevo solo provare
+    print(item)
+
+
+
+     
